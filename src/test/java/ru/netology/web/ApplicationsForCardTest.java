64589985e84f2a48c0a490eaa3bfe03f530ebc64
Tom <gts-2010@mail.ru>
2023-05-12 02:53:15 +0500

    Initial commit

--- src/test/java/ru/netology/web/ApplicationsForCardTest.java
+++ src/test/java/ru/netology/web/ApplicationsForCardTest.java
@@ -45,8 +45,44 @@ class ApplicationsForCardTest {
         driver.findElement(By.cssSelector(".button__text")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
     }
+
+    //НЕГАТИВНЫЕ ТЕСТЫ
+    @Test
+    // Ввод латиницы в фамилии-должно отображаться предупреждение при вводе на латинице
+    void shouldDisplayWarningWhenEnteringInLatin1() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Uнтонов Юрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+     // Ввод латиницы в имени-должно отображаться предупреждение при вводе на латинице
+    void shouldDisplayWarningWhenEnteringInLatin2() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Yрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+     //следует проверить телефон без +
+    void shouldCheckPhoneWithoutPlus() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Антонов Юрий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("71234567890");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector(".button__text")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
 }
-
-
